f726c3675b65ada9718c6b97c0f36b6e22ce40ee
h2chien <h2chien4work@gmail.com>
2022-07-06 22:52:31 +0700

    HHC add

--- src/historicalFigure/NationalHero.java
+++ src/historicalFigure/NationalHero.java
@@ -1,5 +1,37 @@
 package historicalFigure;
 
 public class NationalHero extends HistoricalFigure {
+	private String hometown;
+	private String dynasty;
 
+	public NationalHero(String label) {
+		super(label);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		super.print();
+		System.out.println("Kind of historical figure: National Hero");
+		System.out.println("Hometown: " + this.hometown);
+		System.out.println("Dynasty: " + this.dynasty);
+	}
+
+	public String getHometown() {
+		return hometown;
+	}
+
+	public void setHometown(String hometown) {
+		this.hometown = hometown;
+	}
+
+	public String getDynasty() {
+		return dynasty;
+	}
+
+	public void setDynasty(String dynasty) {
+		this.dynasty = dynasty;
+	}
+	
 }
