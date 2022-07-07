7f74d9bcd3e7a35c8346423d0e688fa8c44a2159
luongnamkhanh <94619689+luongnamkhanh@users.noreply.github.com>
2022-07-07 12:46:13 +0700

    Add files via upload

--- src/attraction/natural/Island.java
+++ src/attraction/natural/Island.java
@@ -1,11 +1,49 @@
 package attraction.natural;
 
-public class Island extends NaturalAttraction {
+import attraction.Attraction;
+
+public class Island extends Attraction {
+	private double totalArea;
+	private double totalPopulation;
+	private double densityPopulation;
+
+	public Island(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void printAttraction() {
 		// TODO Auto-generated method stub
+		super.printAttraction();
+		System.out.println("kind of Natural Attraction: Island");
+		System.out.println("Total area: " + this.totalArea);
+		System.out.println("Total Population: " + this.totalPopulation);
+		System.out.println("Density population: " + this.densityPopulation);
+	}
 
+	public double getTotalArea() {
+		return totalArea;
+	}
+
+	public void setTotalArea(double totalArea) {
+		this.totalArea = totalArea;
+	}
+
+	public double getTotalPopulation() {
+		return totalPopulation;
+	}
+
+	public void setTotalPopulation(double totalPopulation) {
+		this.totalPopulation = totalPopulation;
+	}
+
+	public double getDensityPopulation() {
+		return densityPopulation;
+	}
+
+	public void setDensityPopulation(double densityPopulation) {
+		this.densityPopulation = densityPopulation;
 	}
 
 }
